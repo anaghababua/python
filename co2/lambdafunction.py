f6e46b686fc66e9edcbf72a866de12d285031005
anaghababua <149962251+anaghababua@users.noreply.github.com>
2023-12-01 14:09:02 +0530

    Add files via upload

---
+++ co2/lambdafunction.py
@@ -0,0 +1,14 @@
+l=int(input("enter a length"))
+b=int(input("enter a breadth"))
+rectangle=lambda l,b:l*b
+print(rectangle(l,b))
+
+
+a=int(input("enter the side of square"))
+square=lambda a:a*a
+print("area is:",square(a))
+
+b=int(input("enter the breadth"))
+h=int(input("enter the height"))
+triangle=lambda b,h,:1/2*b*h
+print("area is:",triangle(b,h))
